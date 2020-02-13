73e842343821cb7eead8970a98c0b63ccc7f8558
aemann01 <aemann01@ou.edu>
2020-02-13 11:30:13 -0600

    kraken x3

--- scripts/processing.sh
+++ scripts/processing.sh
@@ -1,90 +1,122 @@
 #Data processing for dietary analysis of calculus paper
 
-cd /home/lymelab/lab_members/mann/diet_calculus
 ###################
 #ENVIRONMENT SETUP
 ###################
-#TO DO -- generate folders
+mkdir samples sim mock_euks mock_oral kraken1 examples sim/bact sim/endo sim/cont adapterremoval
 
 ###############
 #DOWNLOAD DATA
 ###############
-#TO DO -- wget pull data
+echo "Downloading data"
+cd mock_euks
+wget -i ../euk.ids -q &
+cd ../examples 
+wget -i ../examples.ids -q & ## TO DO--> direct download weyrich data
+cd ../mock_oral
+wget -i ../bac.ids -q
+echo "Download complete"
+
+###############
+#TAG SEQUENCES
+###############
+name=$(awk '{print $2}' test.txt)
+file=$(awk '{print $1}' test.txt)
+tagSeq()
+{
+    set $file
+    for i in $name; do
+        sed -i "s/^>.*/>${i}/" ${1}
+        shift
+    done
+}
+tagSeq
+cat *fna > mock_oral.fa
+rm GCF_00*fna
+#now run on eukaryotic genomes
+cd ../mock_euks
+name=$(awk '{print $2}' test.txt)
+file=$(awk '{print $1}' test.txt)
+tagSeq
+ls *gz | parallel 'gzip -d {}'
+# wheat has to be processed separately -- 14G genome compared to 1-2G
+mv GCA_900519105.1_iwgsc_refseqv1.0_genomic.fna ../sim/endo/
+#everyone else concatenated into single fasta
+cat *fna > mock_euks.fa
+rm GC*fna &
+cd ..
 
 #######################
 #SIMULATED ANCIENT DNA
 #######################
 echo "Running Gargammel"
-#generate mock oral community
-perl /home/lymelab/gargammel/gargammel.pl -n 5002000 --comp 0,0,1 --minsize 25 --maxsize 125 -damage 0.03,0.4,0.01,0.3 --loc 4.106487474 --scale 0.358874723 -o sim/mock_oral_simulated sim
-#generate mock dietary reads
-mv mock_oral/mock_oral_raw* sim/endo/
-mv sim/endo/mock_oral_raw.fa* mock_oral/
-mv all_euks.fna* sim/endo/
-perl /home/lymelab/gargammel/gargammel.pl -n 5002000 --comp 0,0,1 --minsize 25 --maxsize 125 -damage 0.03,0.4,0.01,0.3 --loc 4.106487474 --scale 0.358874723 -o sim/mock_euks_simulated sim
-mv sim/end/all_euks* mock_euks
+echo "Generating aDNA from Eukaryotic genomes"
+#first generate reads for wheat
+cp mock_oral/mock_oral.fa sim/bact/
+cp mock_oral/mock_oral.fa sim/cont/
+perl /home/lymelab/gargammel/gargammel.pl -n 1000000 --comp 0,0,1 --minsize 25 --maxsize 125 -damage 0.03,0.4,0.01,0.3 --loc 4.106487474 --scale 0.358874723 -o sim/wheat_sim sim
+rm /sim/endo/GCA_900519105.1_iwgsc_refseqv1.0_genomic.fna*
+cp mock_euks/mock_euks.fa sim/endo/
+#now generate reads for all other euk genomes
+perl /home/lymelab/gargammel/gargammel.pl -n 5005000 --comp 0,0,1 --minsize 25 --maxsize 125 -damage 0.03,0.4,0.01,0.3 --loc 4.106487474 --scale 0.358874723 -o sim/mock_euks_sim sim
+echo "Generating aDNA from Bacterial/Archaeal genomes"
+rm sim/endo/mock_euks.fa
+cp mock_oral/mock_oral.fa sim/endo
+#for oral genomes
+perl /home/lymelab/gargammel/gargammel.pl -n 6000000 --comp 0,0,1 --minsize 25 --maxsize 125 -damage 0.03,0.4,0.01,0.3 --loc 4.106487474 --scale 0.358874723 -o sim/mock_oral_sim sim
 
-######################
-#QUALITY FILTER/MERGE
-######################
-echo "Adapter removal and quality trim"
-AdapterRemoval --file1 sim/mock_oral_simulated_s1.fq.gz --file2 sim/mock_oral_simulated_s2.fq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename mock_oral --minlength 35 &
-AdapterRemoval --file1 sim/mock_euks_simulated_s1.fq.gz --file2 sim/mock_euks_simulated_s2.fq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename mock_euks --minlength 35 &
-#weyrich data
-AdapterRemoval --file1 weyrich2017/ELSIDRON1L7_lTACTG_rCTCGA_R1.fastq --file2 weyrich2017/ELSIDRON1L7_lTACTG_rCTCGA_R2.fastq --trimns --trimqualities --minquality 25 --gzip --collapse --basename elsidron1l7 --minlength 25 &
-#velsko data
-AdapterRemoval --file1 velsko2019/JAE016.A0101_R1_humfilt.fastq.gz --file2 velsko2019/JAE016.A0101_R2_humfilt.fastq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename jae016 --minlength 25 
-rm -r adapterremoval
-mkdir adapterremoval
+###########################
+#QUALITY FILTER/TRIM/MERGE
+###########################
+echo "Adapter removal, quality filter, merge reads"
+AdapterRemoval --file1 sim/wheat_sim_s1.fq.gz --file2 sim/wheat_sim_s2.fq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename mock_wheat --minlength 25 &
+AdapterRemoval --file1 sim/mock_euks_sim_s1.fq.gz --file2 sim/mock_euks_sim_s2.fq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename mock_euks --minlength 25 &
+AdapterRemoval --file1 examples/ELSIDRON1L7_lTACTG_rCTCGA_R1.fastq.gz --file2 examples/ELSIDRON1L7_lTACTG_rCTCGA_R2.fastq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename neanderthal --minlength 25 &
+AdapterRemoval --file1 examples/JAE016.A0101_R1_humfilt.fastq.gz --file2 examples/JAE016.A0101_R2_humfilt.fastq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename modHuman --minlength 25 
+AdapterRemoval --file1 sim/mock_oral_sim_s1.fq.gz --file2 sim/mock_oral_sim_s2.fq.gz --trimns --trimqualities --minquality 25 --gzip --collapse --basename mock_oral --minlength 25 
 mv *gz *settings adapterremoval
+rm -r sim
+
+##############################
+#FASTQ TO FASTA/DEREPLICATION
+##############################
+echo "Converting to fasta, dereplication"
+#keeping collapsed, singletons, and high qualtiy forward reads
+ls adapterremoval/*pair1*gz | parallel 'gzip -d {}' &
+ls adapterremoval/*singleton*gz | parallel 'gzip -d {}' &
+ls adapterremoval/*collapsed*gz | parallel 'gzip -d {}' 
+cd adapterremoval
+ls | grep -v "gz" | grep -v "settings" | parallel 'fastq_to_fasta -i {} -o {}.fna'
+ls *.collapsed.fna | sed 's/.collapsed.fna//' | while read line; do cat $line.collapsed.fna $line.collapsed.truncated.fna $line.pair1.fna $line.pair1.truncated.fna $line.singletons.fna $line.singletons.truncated.fna > $line.fa; done 
+rm *fna 
+ls *fa | sed 's/.fa//' | parallel 'vsearch --derep_fulllength {}.fa --output ../{}.uniq.fa'
+cd ..
+rm -r adapterremoval
+mv neanderthal.uniq.fa modHuman.uniq.fa wheat_sim.uniq.fa samples
 
 #####################
 #EUK GENOME SPIKE IN
 #####################
-#fastq to fasta -- keeping only collapsed and collapsed truncated reads
-ls adapterremoval/*collapsed*gz | parallel 'gzip -d {}'
-ls adapterremoval/*collapsed | parallel 'fastq_to_fasta -i {} -o {}.fa'
-
-
-
-
-#rezip fastq
-ls adapterremoval/*collapsed | grep -v ".fa" | parallel 'gzip {}' &
-#concatenate
-ls adapterremoval/*fa | sed 's/.collapsed.*//' | sort | uniq | while read line; do cat $line.collapsed.fa $line.collapsed.truncated.fa > $line.fa; done
-#remove unecessary fa files
-rm adapterremoval/*collapsed*fa 
-#dereplicate
-ls adapterremoval/*fa | sed 's/.fa//' | parallel 'vsearch --derep_fulllength {}.fa --output {}.uniq.fa'
-mv adapterremoval/*uniq.fa .
-rm adapterremoval/*fa
-
-#split euk file by species
-rm -r diet_reads/
-mkdir diet_reads
-grep ">" mock_euks.uniq.fa | awk -F"_" '{print $2}' | sort | uniq | parallel 'grep {} -A 1 mock_euks.uniq.fa > diet_reads/{}.fa'
+echo "Splitting mock Eukaryotes by species"
+rm -r temp
+mkdir temp
+grep ">" mock_euks.uniq.fa | awk -F":" '{print $1}' | sed 's/>.*_//' | sed 's/>//' | sort | uniq | parallel 'grep {} -A 1 mock_euks.uniq.fa > temp/{}.fa'
 echo "How many dietary reads post quality filter and dereplication?"
-grep ">" diet_reads/*fa -c
-
-##########################
-#MOCK ORAL COMMUNITY PREP
-##########################
-#5000 depth
+grep ">" temp/*fa -c
+echo "Mock oral community prep"
+#5k depth
 cat mock_oral.uniq.fa | awk '/^>/ { if(i>0) printf("\n"); i++; printf("%s\t",$0); next;} {printf("%s",$0);} END { printf("\n");}' | shuf | head -n 4995000 | awk '{printf("%s\n%s\n",$1,$2)}' > mock_oral_5k.fa
 #500 depth
 cat mock_oral.uniq.fa | awk '/^>/ { if(i>0) printf("\n"); i++; printf("%s\t",$0); next;} {printf("%s",$0);} END { printf("\n");}' | shuf | head -n 4999500 | awk '{printf("%s\n%s\n",$1,$2)}' > mock_oral_500.fa
 #50 depth
 cat mock_oral.uniq.fa | awk '/^>/ { if(i>0) printf("\n"); i++; printf("%s\t",$0); next;} {printf("%s",$0);} END { printf("\n");}' | shuf | head -n 4999950 | awk '{printf("%s\n%s\n",$1,$2)}' > mock_oral_50.fa
-echo "Mock oral community preparation, 4995000, 499500, 4999950"
+echo "Mock oral files generated. Should be: 4995000, 499500, 4999950"
 grep ">" *oral*5*.fa -c
-
-##############
-#MOCK SAMPLES
-##############
-mkdir samples 
-mv jae016.uniq.fa elsidron1l7.uniq.fa samples 
-cd diet_reads
-ls *fa | sed 's/.fa//' > diet.ids
+echo "Mock Eukaryotic community prep"
+mv mock_wheat.uniq.fa temp
+mv temp/mock_wheat.uniq.fa temp/Taestivum.fa
+cd temp
+ls *fa | sed 's/.fa$//' > diet.ids
 #5000 subsample
 cat diet.ids | while read line; do cat $line.fa | awk '/^>/ { if(i>0) printf("\n"); i++; printf("%s\t",$0); next;} {printf("%s",$0);} END { printf("\n");}' | shuf | head -n 5000 | awk '{printf("%s\n%s\n",$1,$2)}' > $line.5k.fa; done &
 #500 subsample
@@ -97,47 +129,47 @@ cat diet.ids | while read line; do cat $line.500.fa ../mock_oral_500.fa > ../sam
 cat diet.ids | while read line; do cat $line.50.fa ../mock_oral_50.fa > ../samples/$line.50.spike.fa; done
 cd ..
 echo "Generate mock samples, should all be 5000000"
-grep ">" samples/*fa -c
+grep ">" samples/*spike*fa -c
 
-#clean up
-rm -r *5*fa *uniq*fa diet_reads adapterremoval 
-ls samples/*fa | parallel 'gzip {}'
+## TO DO--> put clean up step here, only keep files you need
 
 #######################
 #KRAKEN DATABASE BUILD
 #######################
 #only run this once! takes a long time to build
-# kraken2-build --download-taxonomy --db /home/lymelab/reference_databases/kraken_nt
-# kraken2-build --download-library nt --db /home/lymelab/reference_databases/kraken_nt
-# kraken2-build --build --db /home/lymelab/reference_databases/kraken_nt --kmer-len 45 --threads 10
+# mkdir kraken_ng 
+# kraken2-build --download-taxonomy --db kraken_nt
+# kraken2-build --download-library nt --db kraken_nt
+# kraken2-build --build --db kraken_nt --kmer-len 45 --threads 10
 
 ########
 #KRAKEN
 ########
-mkdir kraken
 cd samples 
-ls *fa.gz | sed  's/.spike.fa.gz//' | while read line; do kraken2 --db ~/reference_databases/kraken_nt/ --threads 8 --unclassified-out ../kraken/$line.unclassified.out --use-names --gzip-compressed --output ../kraken/$line.out $line.spike.fa; done
+ls *fa | parallel 'gzip {}'
+ls *fa.gz | sed  's/.fa.gz//' | while read line; do kraken2 --db ~/reference_databases/kraken_nt/ --threads 8 --use-names --gzip-compressed --output ../kraken1/$line.out $line.fa.gz; done
+#run 2
+mkdir ../kraken2
+ls *spike*fa.gz | sed  's/.fa.gz//' | while read line; do kraken2 --db ~/reference_databases/kraken_nt/ --threads 8 --use-names --gzip-compressed --output ../kraken2/$line.out $line.fa.gz; done
+#run 3
+mkdir ../kraken3 
+ls *spike*fa.gz | sed  's/.fa.gz//' | while read line; do kraken2 --db ~/reference_databases/kraken_nt/ --threads 8 --use-names --gzip-compressed --output ../kraken3/$line.out $line.fa.gz; done
 
+#get summaries and remove kraken output
+cd ../kraken1
+ls *spike*out | sed 's/.spike.out//' | while read line; do awk -F"\t" '{print $2, "\t", $3}' $line.spike.out | sed 's/:.:.*\t/\t/' | sed 's/^M_//' | sed 's/^MT_//' | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/ /\t/' | sed 's/ //' | sed 's/ /_/g' | sed 's/_(taxid_/\t/' | sed 's/)//' > $line.summary; done
+#just want to get taxonomic information from neanderthal and modern human
+awk -F"\t" '{print $3}' modHuman.out | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/ /\t/' | sed 's/ /_/g' > modHuman.summary
+awk -F"\t" '{print $3}' neanderthal.out | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/ /\t/' | sed 's/ /_/g' > neanderthal.summary
+cd ../kraken2
+ls *out | sed 's/.spike.out//' | while read line; do awk -F"\t" '{print $2, "\t", $3}' $line.spike.out | sed 's/:.:.*\t/\t/' | sed 's/^M_//' | sed 's/^MT_//' | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/ /\t/' | sed 's/ //' | sed 's/ /_/g' | sed 's/_(taxid_/\t/' | sed 's/)//' > $line.summary; done
+cd ../kraken3
+ls *out | sed 's/.spike.out//' | while read line; do awk -F"\t" '{print $2, "\t", $3}' $line.spike.out | sed 's/:.:.*\t/\t/' | sed 's/^M_//' | sed 's/^MT_//' | sort | uniq -c | sed 's/^[ \t]*//' | sed 's/ /\t/' | sed 's/ //' | sed 's/ /_/g' | sed 's/_(taxid_/\t/' | sed 's/)//' > $line.summary; done
+cd ..
+ls kraken1/*out | parallel 'gzip {}' & 
+ls kraken2/*out | parallel 'gzip {}' & 
+ls kraken3/*out | parallel 'gzip {}' & 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#add header line
+ls kraken*/*5* | while read line; do sed -i '1 i\count\tquery\tsubject\ttaxid' $line; done
+#merge taxonomy string from ncbi with kraken summaries
